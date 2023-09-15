68bcdf808ee76c0619c7fb92654632e9dcd6032c
Aman-Mandal <002amankumar@gmail.com>
2023-09-15 17:40:38 +0530

    Added multichain components

---
+++ frontend/constants/index.js
@@ -0,0 +1,279 @@
+export const contractAddress = "0xfE72897d882c3b3d3396012684bED9D7a2e60648";
+export const axelarDomains = {
+  "Optimism Goerli": "optimism",
+  "Polygon Mumbai": "Polygon",
+  Avalanche: "avalanche",
+  Arbitrum: "arbitrum",
+  "BSC Testnet": "binance",
+  "FVM Hyperspace": "filecoin",
+};
+
+export const deployerAbi = [
+  {
+    inputs: [],
+    name: "InvalidAddress",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "NotApprovedByGateway",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint8",
+        name: "version",
+        type: "uint8",
+      },
+    ],
+    name: "Initialized",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "salt",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "bytecode",
+        type: "bytes",
+      },
+    ],
+    name: "computeAddress",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "salt",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "bytecode",
+        type: "bytes",
+      },
+    ],
+    name: "deploy",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "salt",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "bytecode",
+        type: "bytes",
+      },
+      {
+        internalType: "bool",
+        name: "initializable",
+        type: "bool",
+      },
+      {
+        internalType: "bytes",
+        name: "initializeData",
+        type: "bytes",
+      },
+    ],
+    name: "deployContract",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "commandId",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "sourceChain",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "sourceAddress",
+        type: "string",
+      },
+      {
+        internalType: "bytes",
+        name: "payload",
+        type: "bytes",
+      },
+    ],
+    name: "execute",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "commandId",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "sourceChain",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "sourceAddress",
+        type: "string",
+      },
+      {
+        internalType: "bytes",
+        name: "payload",
+        type: "bytes",
+      },
+      {
+        internalType: "string",
+        name: "tokenSymbol",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "executeWithToken",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "gasService",
+    outputs: [
+      {
+        internalType: "contract IAxelarGasService",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "gateway",
+    outputs: [
+      {
+        internalType: "contract IAxelarGateway",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "gateway_",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "gasReceiver_",
+        type: "address",
+      },
+    ],
+    name: "initialize",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "destinationAddress",
+        type: "string",
+      },
+      {
+        internalType: "string[]",
+        name: "destinationDomain",
+        type: "string[]",
+      },
+      {
+        internalType: "bytes32",
+        name: "salt",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes",
+        name: "bytecode",
+        type: "bytes",
+      },
+      {
+        internalType: "uint256[]",
+        name: "relayerFee",
+        type: "uint256[]",
+      },
+      {
+        internalType: "bool",
+        name: "initializable",
+        type: "bool",
+      },
+      {
+        internalType: "bytes",
+        name: "initializeData",
+        type: "bytes",
+      },
+      {
+        internalType: "uint256",
+        name: "totalFee",
+        type: "uint256",
+      },
+    ],
+    name: "xDeployer",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
