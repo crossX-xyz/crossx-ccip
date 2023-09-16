69367a6bda22087e338c48ba25d8e9b8e9125f2c
Aman-Mandal <002amankumar@gmail.com>
2023-09-16 14:15:35 +0530

    Updated components for frontend

---
+++ frontend/components/OptimisticVerification/ContractInput.js
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+
+const ContractInput = ({ id, addAddress }) => {
+  const [updateAddress, setUpdateAddress] = useState('');
+  const [updateChain, setUpdateChain] = useState('');
+
+  const updateInformation = (address, chain) => {
+    addAddress(id, address, chain);
+  };
+
+  const updateAddressHandler = (event) => {
+    setUpdateAddress(event.target.value);
+    updateInformation(event.target.value, updateChain);
+  };
+
+  const updateChainHandler = (event) => {
+    if(event.target.value === 'select') return;
+    
+    setUpdateChain(event.target.value);
+    updateInformation(updateAddress, event.target.value);
+  };
+
+  return (
+    <div className="flex rounded-lg items-center">
+      <div>
+        <select
+          name="chain"
+          onChange={updateChainHandler}
+          value={updateChain}
+          className="bg-[#2D2D2D] outline-none px-2 rounded-l-lg py-4"
+        >
+          <option value="select">Select chain</option>
+          <option value="Polygon Mumbai">Polygon</option>
+          <option value="Gnosis">Gnosis</option>
+          <option value="Scroll">Scroll</option>
+          <option value="Optimism">Optimism</option>
+          <option value="zksync">zkSync</option>
+          <option value="mantle">Mantle</option>
+          <option value="FVM">FVM</option>
+
+        </select>
+      </div>
+      <div>
+        <input
+          className="py-4 px-2 bg-[#232323] outline-none w-[420px] text-gray-400 rounded-r-lg  placeholder:text-sm"
+          placeholder="Paste address here..."
+          onChange={updateAddressHandler}
+          value={updateAddress}
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default ContractInput;
